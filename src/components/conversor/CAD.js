b4c499652ec34d475855d355e1ae007416afc6ff
ChromeCrysis <anderson.bechelli@gmail.com>
2021-04-08 20:44:17 -0300

    alterações scroll_text

--- src/components/conversor/CAD.js
+++ src/components/conversor/CAD.js
@@ -4,13 +4,33 @@ import api from '../../services/api'
 export default class CAD extends React.Component {
     state = {
         CAD: {},
+        valor: 1,
+        result: null,
     }
+    constructor(props){
+        super(props)
+        this.handleValor = this.handleValor.bind(this)
+    }
+    
 
     async componentDidMount(){
         const res = await api.get('/all')
 
         this.setState({ CAD : res.data.CAD })
-        console.log(this.state.CAD)
+        //console.log(this.state.CAD)
+    }
+
+    async handleValor(event){
+        this.setState({
+            valor: Number(event.target.value)
+        })
+    }
+
+    async calcula(){
+        this.setState({
+            result: this.state.valor * this.state.CAD
+        })
+        console.log(this.state.valor)
     }
 
     render() {
@@ -31,6 +51,32 @@ export default class CAD extends React.Component {
                                     </div>
                                 </div>
                             </div>
+                            <br></br>
+                            <hr style={{
+                                backgroundColor: 'black'
+                            }}/>
+                            <hr style={{
+                                backgroundColor: 'black'
+                            }}/>
+                        <div>
+                            <h1>Calculadora</h1>
+                            <div className="row">
+                                <label for="#moedaA">{CAD.code}</label>
+                                <div className="col">
+                                    <input id="moedaA" className="col-md input form-control" type="number"
+                                    defaultValue={this.state.valor} onChange={this.handleValor}/>
+                                    <button onClick={() => this.calcula()}>Calcular</button>
+                                </div>
+                                <div className="col">
+                                    <h2>=</h2>
+                                </div>
+                                <label for="#moedaB">{CAD.codein}</label>
+                                <div className="col">
+                                    <input id="moedaB" className="col-md input form-control" type="number"
+                                        onChange={this.state.result} readOnly="true"/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ): (
                     <h1>Carregando...</h1>
--- src/components/navbar/scroll_text.js
+++ src/components/navbar/scroll_text.js
@@ -29,22 +29,43 @@ export default class ScrollText extends React.Component {
     }
 
     render(){
-        const {ARS, BTC, CAD, CNY, DOGE, EUR, GBO, JPY, USD, USDT} = this.state
+        const {ARS, BTC, CAD, CNY, DOGE, EUR, GBP, JPY, USD, USDT} = this.state
         return(
             <div className="row bg-dark" style={{
                 'border-top': '4px solid #fff'
             }}>
                 <S.ScrollText className="container"> 
-                    {ARS, BTC, CAD ? (
+                    {ARS, BTC, CAD, CNY, DOGE, EUR, GBP, JPY, USD, USDT ? (
                         <div className="row">
                             <div className="col">
-                                <p>Peso Argentino: R$:{ARS.ask}</p>
+                                <p>ARS: R$:{ARS.ask}</p>
                             </div>
                             <div className="col">
-                                <p>Dolar Canadense: R$:{CAD.ask}</p>
+                                <p>CAD: R$:{CAD.ask}</p>
                             </div>
                             <div className="col">
-                                <p>Bitcoin: R$:{BTC.ask}</p>
+                                <p>BTC: R$:{BTC.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>CNY: R$:{CNY.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>DOGE: R$:{DOGE.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>EUR: R$:{EUR.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>GBP: R$:{GBP.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>JPY: R$:{JPY.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>USD: R$:{USD.ask}</p>
+                            </div>
+                            <div className="col">
+                                <p>USDT: R$:{USDT.ask}</p>
                             </div>
                         </div>
                     ) : (
